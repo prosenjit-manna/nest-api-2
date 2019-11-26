583f5cc1c7ef232d61b52ed93fb9c7f787858f67
Prosenjit Manna <bnprosenjit@gmail.com>
2019-11-27 01:29:41 +0530

    user controller update with dto

--- src/dto/create-user.dto.ts
+++ src/dto/create-user.dto.ts
@@ -1,4 +1,14 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiModelProperty()
   readonly emailId: string;
+
+  @ApiModelProperty()
   readonly password: string;
+
+  @ApiModelProperty({
+    required: false,
+  })
+  readonly name: string;
 }
--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,5 +1,7 @@
+import { ApiModelProperty } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UserDto extends CreateUserDto {
+  @ApiModelProperty()
   readonly id: string;
 }
